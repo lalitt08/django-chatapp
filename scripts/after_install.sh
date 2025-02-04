fac5b3649f5ecdd0dc71c76ce811adb3024c4261
Lalit Sangore <96982204+lalitt08@users.noreply.github.com>
2025-02-04 10:31:34 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-echo "Running after install..."
 
-# Navigate to the project directory
+set -e  # Exit on errors
+
+# Navigate to the app directory
 cd /Django_Chatapp/fundoo
-
-# Activate the virtual environment
 source /Django_Chatapp/venv/bin/activate
-
-# Install Python dependencies
-echo "Installing dependencies..."
-pip install -r requirements.txt
-source ~/.bashrc
 # Run database migrations
-echo "Running migrations..."
-python manage.py migrate
+python3 manage.py makemigrations
+bash ~/.bashrc
 
-# Collect static files (if needed)
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-# Set permissions for the start script
-chmod +x /Django_Chatapp/fundoo/start.sh
+# Restart Django service
+sudo systemctl restart django-backend.service
+sudo systemctl status django-backend.service
